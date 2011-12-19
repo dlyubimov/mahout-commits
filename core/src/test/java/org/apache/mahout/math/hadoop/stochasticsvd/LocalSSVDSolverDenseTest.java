4c9dd153f23d277e38821d73ee9eb9e2f9785043
Dmitriy Lyubimov <dlyubimov@inadco.com>
2011-12-18 20:58:45 -0800

    Revert "DEBUG"
    This reverts commit 2561a8541dc8f09f880b092f492713c56413c717.

--- core/src/test/java/org/apache/mahout/math/hadoop/stochasticsvd/LocalSSVDSolverDenseTest.java
+++ core/src/test/java/org/apache/mahout/math/hadoop/stochasticsvd/LocalSSVDSolverDenseTest.java
@@ -132,7 +132,7 @@ public class LocalSSVDSolverDenseTest extends MahoutTestCase {
     ssvd.setAbtBlockHeight(400);
     ssvd.setOverwrite(true);
     ssvd.setQ(q);
-    ssvd.setBroadcast(true);
+    ssvd.setBroadcast(false);
     ssvd.run();
 
     double[] stochasticSValues = ssvd.getSingularValues();
