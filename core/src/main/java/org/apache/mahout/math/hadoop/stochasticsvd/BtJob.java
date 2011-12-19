27f3af3ba4ee3334a1d297946086c5d60d4e962a
Dmitriy Lyubimov <dlyubimov@inadco.com>
2011-12-18 22:33:48 -0800

    debug

--- core/src/main/java/org/apache/mahout/math/hadoop/stochasticsvd/BtJob.java
+++ core/src/main/java/org/apache/mahout/math/hadoop/stochasticsvd/BtJob.java
@@ -28,6 +28,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.filecache.DistributedCache;
 import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.LocalFileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -202,11 +203,15 @@ public final class BtJob {
         Path[] rFiles =
           DistributedCache.getLocalCacheFiles(context.getConfiguration());
 
+        Validate.notNull(rFiles,
+                         "no RHat files in distributed cache job definition");
+
         rhatInput =
           new SequenceFileDirValueIterator<VectorWritable>(rFiles,
                                                            SSVDSolver.PARTITION_COMPARATOR,
                                                            true,
-                                                           new Configuration());
+                                                           FileSystem.getLocal(new Configuration())
+                                                                     .getConf());
 
       } else {
         Path rPath = new Path(qJobPath, QJob.OUTPUT_RHAT + "-*");
