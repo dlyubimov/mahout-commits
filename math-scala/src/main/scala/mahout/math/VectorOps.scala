5eed8ea073de529890c01579038513687db01ae9
Dmitriy Lyubimov <dlyubimov@apache.org>
2013-06-21 23:21:47 -0700

    adding scala module for syntactic sugary drinks

---
+++ math-scala/src/main/scala/mahout/math/VectorOps.scala
@@ -0,0 +1,11 @@
+package mahout.math
+
+import org.apache.mahout.math.Vector
+
+/**
+ * Syntactic sugar for mahout vectors
+ * @param v Mahout vector
+ */
+class VectorOps(val v:Vector) {
+
+}
---
+++ math-scala/src/test/scala/mahout/math/VectorOpsTest.scala
@@ -0,0 +1,12 @@
+package mahout.math
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dmitriy
+ * Date: 6/21/13
+ * Time: 10:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class VectorOpsTest {
+
+}
