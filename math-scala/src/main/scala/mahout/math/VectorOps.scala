a1016630ed266af6de519d7a0283aee6b0d5feb8
Dmitriy Lyubimov <dlyubimov@apache.org>
2013-07-02 22:12:30 -0700

    style

--- math-scala/src/main/scala/mahout/math/VectorOps.scala
+++ math-scala/src/main/scala/mahout/math/VectorOps.scala
@@ -6,17 +6,17 @@ import org.apache.mahout.math.Vector
  * Syntactic sugar for mahout vectors
  * @param v Mahout vector
  */
-class VectorOps(val v:Vector) {
+class VectorOps(val v: Vector) {
 
-  def apply(i:Int) = v.get(i)
+  def apply(i: Int) = v.get(i)
 
   def sum = v.zSum()
 
-  def := (that:Vector) = v.assign(that)
+  def :=(that: Vector) = v.assign(that)
 
   def length = v.size()
 
-  def cloned:Vector = {
+  def cloned: Vector = {
     val vnew = v.like()
     vnew := v
     vnew
