8e91cc6ae5056b159fa6208a26b5a4f0e002bd8c
Dmitriy Lyubimov <dlyubimov@apache.org>
2013-08-07 13:12:36 -0700

    test fixes

--- core/src/test/java/org/apache/mahout/math/hadoop/stochasticsvd/LocalSSVDPCASparseTest.java
+++ core/src/test/java/org/apache/mahout/math/hadoop/stochasticsvd/LocalSSVDPCASparseTest.java
@@ -34,24 +34,54 @@ import org.apache.hadoop.io.compress.DefaultCodec;
 import org.apache.mahout.common.IOUtils;
 import org.apache.mahout.common.MahoutTestCase;
 import org.apache.mahout.common.RandomUtils;
-import org.apache.mahout.math.DenseMatrix;
-import org.apache.mahout.math.DenseVector;
-import org.apache.mahout.math.SequentialAccessSparseVector;
-import org.apache.mahout.math.SingularValueDecomposition;
-import org.apache.mahout.math.Vector;
-import org.apache.mahout.math.VectorWritable;
+import org.apache.mahout.math.*;
+import org.apache.mahout.math.function.DoubleFunction;
 import org.apache.mahout.math.function.Functions;
 import org.junit.Test;
 
 import com.google.common.io.Closeables;
 
-public class LocalSSVDPCADenseTest extends MahoutTestCase {
+public class LocalSSVDPCASparseTest extends MahoutTestCase {
 
   private static final double s_epsilon = 1.0E-10d;
 
   @Test
+  public void testOmegaTRightMultiply() {
+    final Random rnd = RandomUtils.getRandom();
+    final long seed = rnd.nextLong();
+    final int n = 2000;
+
+    final int kp = 100;
+
+    final Omega omega = new Omega(seed, kp);
+    final Matrix materializedOmega = new DenseMatrix(n, kp);
+    for (int i = 0; i < n; i++)
+      for (int j = 0; j < kp; j++)
+        materializedOmega.setQuick(i, j, omega.getQuick(i, j));
+    Vector xi = new DenseVector(n);
+    xi.assign(new DoubleFunction() {
+      @Override
+      public double apply(double x) {
+        return rnd.nextDouble() * 100;
+      }
+    });
+
+    Vector s_o = omega.mutlithreadedTRightMultiply(xi);
+
+    Matrix xiVector = new DenseMatrix(n, 1);
+    xiVector.assignColumn(0, xi);
+
+    Vector s_o_control = materializedOmega.transpose().times(xiVector).viewColumn(0);
+
+    assertEquals(0, s_o.minus(s_o_control).aggregate(Functions.PLUS, Functions.ABS), 1e-10);
+
+    System.out.printf("s_omega=\n%s\n", s_o);
+    System.out.printf("s_omega_control=\n%s\n", s_o_control);
+  }
+
+  @Test
   public void runPCATest1() throws IOException {
-    runSSVDSolver(1);
+    runSSVDSolver(0);
   }
 
   public void runSSVDSolver(int q) throws IOException {
@@ -94,19 +124,19 @@ public class LocalSSVDPCADenseTest extends MahoutTestCase {
     double muAmplitude = 50.0;
     for (int i = 0; i < m; i++) {
       Vector dv = new SequentialAccessSparseVector(n);
+      NamedVector namedRow = new NamedVector(dv,"row-"+i);
       for (int j = 0; j < n * percent / 100; j++) {
         dv.setQuick(rnd.nextInt(n), muAmplitude * (rnd.nextDouble() - 0.25));
       }
       roww.set(i);
-      vw.set(dv);
+      vw.set(namedRow);
       w.append(roww, vw);
       xi.assign(dv, Functions.PLUS);
     }
     closeables.remove(w);
     Closeables.close(w, false);
 
-    // TODO fix test so that 1.0/m works as intended!
-    xi.assign(Functions.mult(1 / m));
+    xi.assign(Functions.mult(1.0 / m));
 
     FileSystem fs = FileSystem.get(conf);
 
@@ -154,20 +184,18 @@ public class LocalSSVDPCADenseTest extends MahoutTestCase {
     Vector stochasticSValues = ssvd.getSingularValues();
     System.out.println("--SSVD solver singular values:");
     LocalSSVDSolverSparseSequentialTest.dumpSv(stochasticSValues);
-    System.out.println("--Colt SVD solver singular values:");
+    System.out.println("--SVD solver singular values:");
 
     // try to run the same thing without stochastic algo
-    double[][] a = SSVDHelper.loadDistributedRowMatrix(fs, aPath, conf);
+    Matrix a = new DenseMatrix(SSVDHelper.loadDistributedRowMatrix(fs, aPath, conf));
 
     // subtract pseudo pca mean
     for (int i = 0; i < m; i++) {
-      for (int j = 0; j < n; j++) {
-        a[i][j] -= xi.getQuick(j);
-      }
+      a.viewRow(i).assign(xi,Functions.MINUS);
     }
 
     SingularValueDecomposition svd2 =
-      new SingularValueDecomposition(new DenseMatrix(a));
+      new SingularValueDecomposition(a);
 
     Vector svalues2 = new DenseVector(svd2.getSingularValues());
     LocalSSVDSolverSparseSequentialTest.dumpSv(svalues2);
