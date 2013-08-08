412f9c5e06bf2a510948475e7d670f91d18d3386
Dmitriy Lyubimov <dlyubimov@apache.org>
2013-08-07 22:36:17 -0700

    cleaning up certain things; pca tests are asserting names and keys propagation

--- core/src/test/java/org/apache/mahout/math/hadoop/stochasticsvd/LocalSSVDPCASparseTest.java
+++ core/src/test/java/org/apache/mahout/math/hadoop/stochasticsvd/LocalSSVDPCASparseTest.java
@@ -25,9 +25,12 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.SequenceFile.CompressionType;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.io.compress.DefaultCodec;
 import org.apache.mahout.common.IOUtils;
 import org.apache.mahout.common.MahoutTestCase;
+import org.apache.mahout.common.Pair;
 import org.apache.mahout.common.RandomUtils;
 import org.apache.mahout.math.*;
 import org.apache.mahout.math.function.DoubleFunction;
@@ -39,6 +42,7 @@ import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
 import java.util.Deque;
+import java.util.Iterator;
 import java.util.Random;
 
 public class LocalSSVDPCASparseTest extends MahoutTestCase {
@@ -84,6 +88,12 @@ public class LocalSSVDPCASparseTest extends MahoutTestCase {
     runSSVDSolver(1);
   }
 
+  @Test
+  public void runPCATest0() throws IOException {
+    runSSVDSolver(0);
+  }
+
+
   public void runSSVDSolver(int q) throws IOException {
 
     Configuration conf = new Configuration();
@@ -94,127 +104,149 @@ public class LocalSSVDPCASparseTest extends MahoutTestCase {
     // conf.set("fs.default.name","hdfs://localhost:11010/");
 
     Deque<Closeable> closeables = Lists.newLinkedList();
-    Random rnd = RandomUtils.getRandom();
+    try {
+      Random rnd = RandomUtils.getRandom();
 
-    File tmpDir = getTestTempDir("svdtmp");
-    conf.set("hadoop.tmp.dir", tmpDir.getAbsolutePath());
+      File tmpDir = getTestTempDir("svdtmp");
+      conf.set("hadoop.tmp.dir", tmpDir.getAbsolutePath());
 
-    Path aLocPath = new Path(getTestTempDirPath("svdtmp/A"), "A.seq");
+      Path aLocPath = new Path(getTestTempDirPath("svdtmp/A"), "A.seq");
 
-    // create distributed row matrix-like struct
-    SequenceFile.Writer w =
-      SequenceFile.createWriter(FileSystem.getLocal(conf),
-                                conf,
-                                aLocPath,
-                                IntWritable.class,
-                                VectorWritable.class,
-                                CompressionType.BLOCK,
-                                new DefaultCodec());
-    closeables.addFirst(w);
+      // create distributed row matrix-like struct
+      SequenceFile.Writer w =
+        SequenceFile.createWriter(FileSystem.getLocal(conf),
+                                  conf,
+                                  aLocPath,
+                                  Text.class,
+                                  VectorWritable.class,
+                                  CompressionType.BLOCK,
+                                  new DefaultCodec());
+      closeables.addFirst(w);
 
-    int n = 100;
-    int m = 2000;
-    double percent = 5;
+      int n = 100;
+      int m = 2000;
+      double percent = 5;
 
-    VectorWritable vw = new VectorWritable();
-    IntWritable roww = new IntWritable();
+      VectorWritable vw = new VectorWritable();
+      Text rkey = new Text();
 
-    Vector xi = new DenseVector(n);
+      Vector xi = new DenseVector(n);
 
-    double muAmplitude = 50.0;
-    for (int i = 0; i < m; i++) {
-      Vector dv = new SequentialAccessSparseVector(n);
-      NamedVector namedRow = new NamedVector(dv, "row-" + i);
-      for (int j = 0; j < n * percent / 100; j++) {
-        dv.setQuick(rnd.nextInt(n), muAmplitude * (rnd.nextDouble() - 0.25));
+      double muAmplitude = 50.0;
+      for (int i = 0; i < m; i++) {
+        Vector dv = new SequentialAccessSparseVector(n);
+        String rowname = "row-"+i;
+        NamedVector namedRow = new NamedVector(dv, rowname);
+        for (int j = 0; j < n * percent / 100; j++) {
+          dv.setQuick(rnd.nextInt(n), muAmplitude * (rnd.nextDouble() - 0.25));
+        }
+        rkey.set("row-i"+i);
+        vw.set(namedRow);
+        w.append(rkey, vw);
+        xi.assign(dv, Functions.PLUS);
       }
-      roww.set(i);
-      vw.set(namedRow);
-      w.append(roww, vw);
-      xi.assign(dv, Functions.PLUS);
-    }
-    closeables.remove(w);
-    Closeables.close(w, false);
+      closeables.remove(w);
+      Closeables.close(w, false);
 
-    xi.assign(Functions.mult(1.0 / m));
+      xi.assign(Functions.mult(1.0 / m));
 
-    FileSystem fs = FileSystem.get(conf);
+      FileSystem fs = FileSystem.get(conf);
 
-    Path tempDirPath = getTestTempDirPath("svd-proc");
-    Path aPath = new Path(tempDirPath, "A/A.seq");
-    fs.copyFromLocalFile(aLocPath, aPath);
-    Path xiPath = new Path(tempDirPath, "xi/xi.seq");
-    SSVDHelper.saveVector(xi, xiPath, conf);
+      Path tempDirPath = getTestTempDirPath("svd-proc");
+      Path aPath = new Path(tempDirPath, "A/A.seq");
+      fs.copyFromLocalFile(aLocPath, aPath);
+      Path xiPath = new Path(tempDirPath, "xi/xi.seq");
+      SSVDHelper.saveVector(xi, xiPath, conf);
 
-    Path svdOutPath = new Path(tempDirPath, "SSVD-out");
+      Path svdOutPath = new Path(tempDirPath, "SSVD-out");
 
-    // make sure we wipe out previous test results, just a convenience
-    fs.delete(svdOutPath, true);
+      // make sure we wipe out previous test results, just a convenience
+      fs.delete(svdOutPath, true);
 
-    // Solver starts here:
-    System.out.println("Input prepared, starting solver...");
+      // Solver starts here:
+      System.out.println("Input prepared, starting solver...");
 
-    int ablockRows = 867;
-    int p = 60;
-    int k = 40;
-    SSVDSolver ssvd =
-      new SSVDSolver(conf,
-                     new Path[]{aPath},
-                     svdOutPath,
-                     ablockRows,
-                     k,
-                     p,
-                     3);
-    ssvd.setOuterBlockHeight(500);
-    ssvd.setAbtBlockHeight(251);
-    ssvd.setPcaMeanPath(xiPath);
+      int ablockRows = 867;
+      int p = 60;
+      int k = 40;
+      SSVDSolver ssvd =
+        new SSVDSolver(conf,
+                       new Path[]{aPath},
+                       svdOutPath,
+                       ablockRows,
+                       k,
+                       p,
+                       3);
+      ssvd.setOuterBlockHeight(500);
+      ssvd.setAbtBlockHeight(251);
+      ssvd.setPcaMeanPath(xiPath);
 
     /*
-     * removing V,U jobs from this test to reduce running time. i will keep them
+     * Removing V,U jobs from this test to reduce running time. i will keep them
      * put in the dense test though.
+     *
+     * For PCA test, we also want to request U*Sigma output and check it for named
+     * vector propagation.
      */
-    ssvd.setComputeU(false);
-    ssvd.setComputeV(false);
+      ssvd.setComputeU(false);
+      ssvd.setComputeV(false);
+      ssvd.setcUSigma(true);
 
-    ssvd.setOverwrite(true);
-    ssvd.setQ(q);
-    ssvd.setBroadcast(true);
-    ssvd.run();
+      ssvd.setOverwrite(true);
+      ssvd.setQ(q);
+      ssvd.setBroadcast(true);
+      ssvd.run();
 
-    Vector stochasticSValues = ssvd.getSingularValues();
-    System.out.println("--SSVD solver singular values:");
-    LocalSSVDSolverSparseSequentialTest.dumpSv(stochasticSValues);
-    System.out.println("--SVD solver singular values:");
+      Vector stochasticSValues = ssvd.getSingularValues();
 
-    // try to run the same thing without stochastic algo
-    Matrix a = new DenseMatrix(SSVDHelper.loadDistributedRowMatrix(fs, aPath, conf));
+      // try to run the same thing without stochastic algo
+      Matrix a = SSVDHelper.drmLoadAsDense(fs, aPath, conf);
 
-    verifyInternals(svdOutPath, a, new Omega(ssvd.getOmegaSeed(), k + p), k + p, q);
+      verifyInternals(svdOutPath, a, new Omega(ssvd.getOmegaSeed(), k + p), k + p, q);
 
-    // subtract pseudo pca mean
-    for (int i = 0; i < m; i++) {
-      a.viewRow(i).assign(xi, Functions.MINUS);
+      // subtract pseudo pca mean
+      for (int i = 0; i < m; i++) {
+        a.viewRow(i).assign(xi, Functions.MINUS);
+      }
+
+      SingularValueDecomposition svd2 =
+        new SingularValueDecomposition(a);
+
+      Vector svalues2 = new DenseVector(svd2.getSingularValues());
+
+      System.out.println("--SSVD solver singular values:");
+      LocalSSVDSolverSparseSequentialTest.dumpSv(stochasticSValues);
+      System.out.println("--SVD solver singular values:");
+      LocalSSVDSolverSparseSequentialTest.dumpSv(svalues2);
+
+      for (int i = 0; i < k + p; i++) {
+        assertTrue(Math.abs(svalues2.getQuick(i) - stochasticSValues.getQuick(i)) <= s_epsilon);
+      }
+
+      DenseMatrix mQ =
+        SSVDHelper.drmLoadAsDense(fs, new Path(svdOutPath, "Bt-job/"
+          + BtJob.OUTPUT_Q + "-*"), conf);
+
+      SSVDCommonTest.assertOrthonormality(mQ,
+                                          false,
+                                          s_epsilon);
+
+      // assert name propagation
+      for (Iterator<Pair<Writable, Vector>> iter = SSVDHelper.drmIterator(fs,
+                                                                          new Path(ssvd.getuSigmaPath()+"/*"),
+                                                                          conf,
+                                                                          closeables); iter.hasNext(); ) {
+        Pair<Writable, Vector> pair = iter.next();
+        Writable key = pair.getFirst();
+        Vector v = pair.getSecond();
+
+        assertTrue(v instanceof NamedVector);
+        assertTrue(key instanceof Text);
+      }
+
+    } finally {
+      IOUtils.close(closeables);
     }
-
-    SingularValueDecomposition svd2 =
-      new SingularValueDecomposition(a);
-
-    Vector svalues2 = new DenseVector(svd2.getSingularValues());
-    LocalSSVDSolverSparseSequentialTest.dumpSv(svalues2);
-
-    for (int i = 0; i < k + p; i++) {
-      assertTrue(Math.abs(svalues2.getQuick(i) - stochasticSValues.getQuick(i)) <= s_epsilon);
-    }
-
-    double[][] mQ =
-      SSVDHelper.loadDistributedRowMatrix(fs, new Path(svdOutPath, "Bt-job/"
-        + BtJob.OUTPUT_Q + "-*"), conf);
-
-    SSVDCommonTest.assertOrthonormality(new DenseMatrix(mQ),
-                                        false,
-                                        s_epsilon);
-
-    IOUtils.close(closeables);
   }
 
   private void verifyInternals(Path tempDir, Matrix a, Omega omega, int kp, int q) {
@@ -236,12 +268,12 @@ public class LocalSSVDPCASparseTest extends MahoutTestCase {
 
     Vector s_o = omega.mutlithreadedTRightMultiply(xi);
 
-    System.out.printf("s_omega=\n%s\n",s_o);
+    System.out.printf("s_omega=\n%s\n", s_o);
 
     Matrix y = a.times(momega);
-    for (int i = 0; i < n; i++ ) y.viewRow(i).assign(s_o,Functions.MINUS);
+    for (int i = 0; i < n; i++) y.viewRow(i).assign(s_o, Functions.MINUS);
 
-    QRDecomposition qr= new QRDecomposition(y);
+    QRDecomposition qr = new QRDecomposition(y);
     Matrix qm = qr.getQ();
 
     Vector s_q = qm.aggregateColumns(new VectorFunction() {
@@ -251,13 +283,13 @@ public class LocalSSVDPCASparseTest extends MahoutTestCase {
       }
     });
 
-    System.out.printf("s_q=\n%s\n",s_q);
+    System.out.printf("s_q=\n%s\n", s_q);
 
     Matrix b = qm.transpose().times(a);
 
     Vector s_b = b.times(xi);
 
-    System.out.printf("s_b=\n%s\n",s_b);
+    System.out.printf("s_b=\n%s\n", s_b);
 
 
   }
