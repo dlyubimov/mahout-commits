25f9cb81a355814529f72616aee6b58ad7c204d4
Dmitriy Lyubimov <dlyubimov@apache.org>
2013-11-25 11:34:57 -0800

    WIP migrating out weighted ALS

---
+++ spark/src/main/scala/org/apache/mahout/sparkbindings/als/WeightedALSWR.scala
@@ -0,0 +1,337 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.mahout.sparkbindings.als
+
+import org.apache.log4j.Logger
+import org.apache.mahout.sparkbindings.drm.{BaseDRM, VectorMessage, DRM}
+import org.apache.mahout.math._
+import org.apache.spark.bagel.Bagel
+import org.apache.mahout.math._
+import org.apache.mahout.math.RLikeOps._
+import collection.mutable
+import scala.collection.JavaConversions._
+import org.apache.spark.storage.StorageLevel
+import scala.math._
+import org.apache.spark.SparkContext._
+import collection.mutable.ArrayBuffer
+import scala.util.Random
+import org.apache.spark.rdd.RDD
+
+/**
+ *
+ * This is ALS-WR with confidence. See distributed-als-with-confidence.pdf for details.
+ *
+ * Inputs are P and C DRM matrices.
+ *
+ */
+object WeightedALSWR {
+
+  private val s_log = Logger.getLogger(WeightedALSWR.getClass)
+
+  /**
+   * Weighted ALS-WR
+   * @param P preference matrix (1 or 0's)
+   * @param C confidence matrix C' = C-C_0
+   * @param k desired decomposition rank
+   * @param lambda L2 regularization rate
+   * @param niter maximum number of iterations
+   * @return (U,V) as drm matrices.
+   */
+  def walswr(P: DRM[Int], C: DRM[Int], k: Int, lambda: Double = 1e-6, niter: Int = 10):
+  (DRM[Int], DRM[Int]) = {
+
+    // Make sure P and C cached, since we will have multiple passes over same input to calculate
+    // errors.
+
+    P.cached
+    C.cached
+
+    val nusers = P.nrow.toInt
+    val nitems = P.ncol
+
+    if (nusers != P.nrow)
+      throw new IllegalArgumentException("Can't handle input with more than MAXINT (~2bln) columns")
+
+    var uverts = initFactors(P, C, k)
+    var vverts = initFactors(P.t, C.t, k)
+
+    val iterRmse = new ArrayBuffer[Double]()
+
+    for (i <- 0 until niter) {
+
+      s_log.info("Weighted ALS-WR Iteration %d.".format(i))
+
+      uverts = alswrIter(uverts.map(t => {
+        t._2.active = true
+        t
+      }), nusers, vverts, nitems, k, lambda)
+
+      //      printf("iter %d U=\n%s\n",i, verts2DRM(uverts,nusers,k).collect)
+
+
+      vverts = alswrIter(vverts.map(t => {
+        t._2.active = true
+        t
+      }), nitems, uverts, nusers, k, lambda)
+
+      iterRmse+=rmse(vverts)
+
+    }
+
+    printf("rmse=%s\n",iterRmse)
+
+    (verts2DRM(uverts, nusers, k), verts2DRM(vverts, nitems, k))
+
+  }
+
+  private def verts2DRM(verts: RDD[(Int, ALSWRCVertex)], nrow: Int, k: Int): DRM[Int] =
+    new BaseDRM[Int](verts.map(t => (t._1, t._2.factorVec)), _nrow = nrow, _ncol = k)
+
+
+  /**
+   * This assumes that every user has a fairly sparse usage history.
+   * If the matrix becomes fairly close to dense, this ALS-WR approach will becomes obviously
+   * fairly inefficient (approaching dense matrix multiplication behavior).
+   *
+   * Therefore it is recommended to purge user histories for abnormally active history.
+   * It is likely it is not typical user anyway, plus it might hamper computations.
+   *
+   * @param uverts
+   * @param nurows
+   * @param vverts
+   * @param nvrows
+   * @param k
+   * @param lambda
+   * @return
+   */
+  private def alswrIter(uverts: RDD[(Int, ALSWRCVertex)],
+                        nurows: Int,
+                        vverts: RDD[(Int, ALSWRCVertex)],
+                        nvrows: Int,
+                        k: Int,
+                        lambda: Double):
+  RDD[(Int, ALSWRCVertex)] = {
+
+    implicit val sc = uverts.context
+    val drmV = verts2DRM(vverts, nvrows, k)
+
+    println("V(%d,%d)".format(drmV.nrow, drmV.ncol))
+
+    val inCoreVtV = drmV.t_sq()
+
+    // look at the rank. if we don't have full rank here,
+    // the system is singular and we would have trouble with our fitting.
+    if (!inCoreVtV.isFullRank)
+      throw new IllegalArgumentException(
+        "The input doesn't have enough information for k-rank ALS. " +
+          "Try to reduce k for more meaningful results.")
+
+    // double-check, really in theory not needed, since we already checked for rank,
+    // but i just want to make sure Cholesky does what it needs to, here. If everything ok
+    // i may remove this double-verification later.
+    val l = chol(inCoreVtV).getL
+    val good = (for (row <- 0 until l.nrow; col <- 0 to row) yield l(row, col)).forall(abs(_) > 1e-13)
+
+
+    if (!good) {
+      if (s_log.isDebugEnabled) {
+        // compute control in-core
+        val icV = drmV.collect
+        val icVtV = icV.t %*% icV
+        val normResidual = (icVtV - inCoreVtV).norm
+        s_log.debug("norm residual:%.2f".format(normResidual))
+        s_log.debug("computed V'V=\n%s\n".format(inCoreVtV))
+        s_log.debug("actual V'V=\n%s\n".format(icVtV))
+        s_log.debug("V=\n%s\n".format(icV))
+
+      }
+      throw new IllegalArgumentException(
+        "V'V computation produces non-positive definite? Cholesky undetermined:L=\n%s\n".format(l))
+    }
+
+    // broadcast new V'V to all nodes.
+    // this should be ok since V'V is just k x k
+    val vtv = sc.broadcast(inCoreVtV)
+
+    val msgs = vverts.flatMap(t => {
+      val outMsgs = new mutable.HashMap[Int, ALSVectorMsg]()
+      val srcId = t._1
+      val vert = t._2
+
+      /* populate out msgs to include rows mentioned for confidence */
+      vert.c.nonZeroes().foreach(el => outMsgs += (el.index ->
+        new ALSVectorMsg(srcId, el.index(), vert.factorVec)))
+
+      /* populate out msgs to include rows mentioned for preferences */
+      vert.p.nonZeroes().foreach(el => if (!outMsgs.contains(el.index))
+        outMsgs += (el.index -> new ALSVectorMsg(srcId, el.index(), vert.factorVec))
+      )
+
+      outMsgs.toTraversable
+    })
+
+    val numPartitions = uverts.partitions.size
+
+    val newUverts = Bagel.run(sc, uverts, msgs, numPartitions, StorageLevel.MEMORY_AND_DISK_SER)((vert, msgs, step) => {
+
+
+      val newU = if (vert.p.getNumNondefaultElements > 0) {
+        // see if we have enough confidence to comprise non-signular VtDV
+        val n_u = vert.c.getNumNondefaultElements
+
+        val icVtV = vtv.value
+
+        // ideally, we might want to form vBlock directly as a full-cardinality SparseColMatrix.
+        // unfortunately this will not work very well with the way currently SparseCol/RowMatrices are
+        // structured in Mahout. I am submitting a few patches that should lift that concern
+        // and make this code much more accessible thru use of sparse matrix blocking rather than dense
+        // blocking with a level of index indirection.
+
+        val msgMap = msgs.toTraversable.flatMap(_.view.map(msg => ((msg.sourceId -> msg.v)))).toMap
+
+        val cIndices = vert.c.nonZeroes().map(_.index)
+        val pIndices = vert.p.nonZeroes().map(_.index)
+
+        val vBlockForC = if (n_u == 0) null
+        else dense(cIndices.map(msgMap(_)).toSeq: _*)
+
+        val vBlockForP = dense(pIndices.map(msgMap(_)).toSeq: _*)
+
+        // diagonal for D
+        val d = if (n_u == 0) null else dvec(vert.c.nonZeroes().view.map(_.get))
+
+        /*
+          this computes V'V + V'DV + n_u*lambda*I, and then takes Cholesky.
+
+          if n_u = 0 then the only thing here to have is V'V
+
+          Note that we sparsify V'DV operation by picking only row vectors
+          from V that have corresponding nonzero diagonal element in D (and thus we throw
+          zero diagonal elements from D as well).
+         */
+
+        val cholArg =
+          if (n_u > 0)
+            icVtV + (vBlockForC.t %*%: diagv(d)) %*% vBlockForC + diag(n_u * lambda, k)
+          else icVtV
+
+        val ch = chol(cholArg)
+
+
+        //c_0 * I + D'
+        val dForP = dvec(pIndices.map(i => vert.c(i) + 1))
+
+        val p = dvec(pIndices.map(i => vert.p(i)))
+
+        // b <- V' * (c_0 I + D(u)) * P(u)
+        // we do this over dense blocks of course skipping irrelevant rows and elements
+        val b = (vBlockForP.t %*%: diagv(dForP)) %*% dense(p).t
+
+        // solve linear system, result is U(V)-row-vector
+        val urow = ch.solveRight(eye(k)) %*% ch.solveLeft(b)
+
+        if (s_log.isDebugEnabled) {
+          // Generally, this is not supposed to be happening for full-rank
+          // matrices. We've asserted full rank vor V'V before broadcasting it,
+          // so with a very great probability Cholesky argument is full rank here.
+          // unless we screwed with computations, in which case it also may cause
+          // non-positive definite input as well.
+          val err = (cholArg %*% urow - b).norm
+          assert(err <= 1e-10,
+            "Cholesky linear system solution failed, residual norm = %.4f.".format(err))
+        }
+
+        // residual computation
+        // we assume that that data we *know* is the one identified by non-zero indices of confidences vert.c.
+        // for each such indices we compute dot-product of newU and corresponding v-row vector.
+        // we can see that these values are represented by values of the vector (vBlockForC %*% newU).
+        // therefore (vert.p remapped to cIndices -  (vBlockForC %*% newU)) will represent a vector of residuals
+
+        vert.residual = if (n_u > 0) {
+          val pForC = dvec(cIndices.map(vert.p(_)))
+
+          val residVec = pForC - (vBlockForC %*% urow)(::, 0)
+
+          // square each item in the vector of residuals to obtain partial mse.
+          // we don't explicitly save num of items because they could be obtained
+          // as n_u = vert.c.getNumNondefaultElements per above at any given moment.
+          (residVec *= residVec) sum
+
+        } else 0.0
+
+        urow
+
+      } else {
+        vert.residual = 0.0
+        new SparseColumnMatrix(k, 1)
+      }
+
+      assert(newU.ncol == 1)
+      assert(newU.nrow == k)
+
+      vert.factorVec = new DenseVector(k) := newU(::, 0)
+
+      vert.active = false
+
+
+      (vert, Array())
+    })
+
+    newUverts
+
+  }
+
+  private def rmse(verts: RDD[(Int, ALSWRCVertex)]) = {
+    val t = verts.map(t => (t._2.c.getNumNondefaultElements.toLong, t._2.residual))
+      .fold(0L, 0.0)((t1, t2) => (t1._1 + t2._1, t1._2 + t2._2))
+    if (t._1 == 0L) 0.0 else sqrt(t._2 / t._1)
+  }
+
+  private def initFactors(P: DRM[Int], C: DRM[Int], k: Int): RDD[(Int, ALSWRCVertex)] = {
+
+    val ncol = P.ncol
+    val sc = P.getRDD.context
+    // important: fix the seed in the front-end and then re-distribute
+    // to ensure idempotency of recomputations.
+    val seed = new Random().nextLong()
+
+
+    val verts = P.getRDD.mapWith(part => new Random(seed + part))((t, rnd) => {
+      val factorVec = new DenseVector(k)
+      for (i <- 0 until k) factorVec.setQuick(i, rnd.nextDouble / 100)
+      val p = t._2
+      val id = t._1
+
+      (id, new ALSWRCVertex(id, factorVec, p, new RandomAccessSparseVector(ncol)))
+    })
+
+    val msgs = C.getRDD.map(t => (t._1, new VectorMessage[Int](t._1, t._2)))
+
+
+    Bagel.run(sc, verts, msgs, P.getRDD.partitions.size)((vert, msgs, step) => {
+
+      msgs.foreach(_.view.foreach(msg => vert.c := msg.v))
+
+      vert.active = false
+
+      (vert, Array())
+
+    })
+  }
+
+}
---
+++ spark/src/test/scala/org/apache/mahout/sparkbindings/als/WeightedALSWRTest.scala
@@ -0,0 +1,141 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.mahout.sparkbindings.als
+
+import org.scalatest.FunSuite
+import org.apache.mahout.math.{SparseMatrix, DenseMatrix, Matrix}
+import util.Random
+import mahout.math._
+import mahout.math.RLikeOps._
+import org.apache.log4j.BasicConfigurator
+
+/**
+  *
+  * @author dmitriy
+  */
+class WeightedALSWRTest extends FunSuite {
+
+   test("on simulated sparse binomial input") {
+
+     BasicConfigurator.resetConfiguration()
+     BasicConfigurator.configure()
+
+     val (p, c, pActual) = createData(40, 50, 10, 0.1)
+
+     printf("P=\n%s\n", p)
+     printf("C=\n%s\n", c)
+
+     implicit val sc = mahoutSparkContext("local", "WeightedALSWRTest")
+     try {
+
+       val (drmP, drmC) = (drmParallelize(p), drmParallelize(c))
+
+       //    printf("C'=\n%s\n", drmC.t.collect)
+       //    printf("P'=\n%s\n", drmP.t.collect)
+
+       val (uDrm, vDrm) = WeightedALSWR.walswr(drmP, drmC, 5, 1e-6, 8)
+       val (u, v) = (uDrm.collect, vDrm.collect)
+
+       printf("U=\n%s\n", u)
+       printf("V=\n%s\n", v)
+
+       printf("P_actual=\n%s\n", pActual)
+       printf("P_predict=\n%s\n", u %*% v.t)
+     } finally {
+       sc.stop()
+     }
+
+   }
+
+   test("train on ideal non-sparse input") {
+
+     val (nrow, ncol) = (40, 50)
+     val k = 10
+
+     val (p, c) = createIdealData(nrow, ncol, k)
+
+     printf("P=\n%s\n", p)
+     printf("C=\n%s\n", c)
+
+     implicit val sc = mahoutSparkContext("local", "WeightedALSWRTest")
+     try {
+
+       val (drmP, drmC) = (drmParallelize(p), drmParallelize(c))
+
+       val (uDrm, vDrm) = WeightedALSWR.walswr(drmP, drmC, k, 1e-6, 8)
+       val (u, v) = (uDrm.collect, vDrm.collect)
+
+       printf("U=\n%s\n", u)
+       printf("V=\n%s\n", v)
+
+       printf("P_actual=\n%s\n", p)
+       printf("P_predict=\n%s\n", u %*% v.t)
+
+     } finally {
+       sc.stop()
+     }
+
+   }
+
+
+   def createIdealData(nrow: Int, ncol: Int, k: Int): (Matrix, Matrix) = {
+
+     val (uSim, vSim) = (new DenseMatrix(nrow, k), new DenseMatrix(ncol, k))
+
+     val rnd = new Random()
+
+     uSim := ((r, c, v) => rnd.nextDouble())
+     vSim := ((r, c, v) => rnd.nextDouble())
+
+     val p = new DenseMatrix(nrow, ncol) := ((r, c, v) => uSim(r, ::) dot vSim(c, ::))
+
+     (p, new SparseMatrix(nrow, ncol))
+   }
+
+
+   def createData(nusers: Int, nitems: Int, k: Int, visibleTrainRatio: Double,
+       goodConfidence: Double = 15): (Matrix, Matrix, Matrix) = {
+
+     val rnd = new Random(123L)
+
+     val (uSim, vSim) = (new DenseMatrix(nusers, k), new DenseMatrix(nitems, k))
+
+     uSim := ((r, c, v) => rnd.nextDouble())
+     vSim := ((r, c, v) => rnd.nextDouble())
+
+     val (p, c, pactual) = (new SparseMatrix(nusers, nitems), new SparseMatrix(nusers, nitems), new SparseMatrix(nusers,
+       nitems))
+
+     for (u <- 0 until nusers; i <- 0 until nitems) {
+
+       val score = uSim(u, ::) dot vSim(i, ::)
+       pactual(u, i) = if (score < 0.25 * k) 1 else 0
+
+       if (rnd.nextDouble < visibleTrainRatio) {
+         p(u, i) = pactual(u, i)
+         c(u, i) = goodConfidence
+       }
+
+     }
+
+
+     (p, c, pactual)
+   }
+
+
+ }
